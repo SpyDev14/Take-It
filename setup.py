a9ff84031855826f127c6bcc3e95d05bb6ce620c
VideoKompany <135313844+SpyDev14@users.noreply.github.com>
2025-04-18 09:11:14 +0400

    remove wrong comment in setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="content",
     version="0.1",
-    packages=find_packages(include=["content*"]),  # Явно включаем только app
+    packages=find_packages(include=["content*"]),
     package_dir={"": "."},  # Ищем пакеты в корне
 )
